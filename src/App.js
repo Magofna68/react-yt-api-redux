bec52fe9f02950f50a144346e92f671027eb3aaa
Brandon-Magofna <magofna68@gmail.com>
2022-01-16 18:22:11 -0800

    add ErrorBoundaries to project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
 import React, {Component} from 'react';
+import YTSearch from 'youtube-search-api';
+import VideoList from './components/VideoList';
 import SearchBar from './components/SearchBar.js';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      video: []
+    }
+    this.videoSearch('music');
+  }
+  videoSearch(searchTerm) {
+    YTSearch({key: `${process.env.REACT_APP_API_KEY}`, term: searchTerm}, (data) => {
+      console.log(data);
+      this.setState({
+        videos: data
+      })
+    })
+  }
   render () {
     return (
       <div>
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class searchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: ''};
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value })
+  }
   render() {
     return (
       <div>
-        <input />
+        <input value={this.state.term} onChange={this.onInputChange} />
       </div>
     );
   }
