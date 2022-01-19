59935c8decdf1d4bea3ccc306adc3f5143e400c6
Brandon-Magofna <magofna68@gmail.com>
2022-01-19 15:58:01 -0800

    finish searchBar component and start styling application

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -16,6 +16,7 @@ class searchBar extends React.Component {
     return (
       <div className='search-bar'>
          <input 
+          placeholder='Search...'
           value={this.state.term} 
           onChange={this.onInputChange} 
         />
