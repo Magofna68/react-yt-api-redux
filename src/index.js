50197c6c58ac2c4b5a9acb97498a1b255d8ff641
Brandon-Magofna <magofna68@gmail.com>
2022-01-21 18:21:22 -0800

    remove ErrorBoundary component; and update styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ErrorBoundary from './components/ErrorBoundary';
 
 ReactDOM.render(
   <React.StrictMode>
