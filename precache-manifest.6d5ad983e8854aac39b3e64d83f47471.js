d74e6e8219eb54c323c292d08c99dde4b5ef8834
Brandon-Magofna <magofna68@gmail.com>
2022-01-24 15:00:13 -0800

    Updates

---
+++ precache-manifest.6d5ad983e8854aac39b3e64d83f47471.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935bd82d1f3ff6c00b0e15f48a75594a",
+    "url": "/react-yt-api-redux/index.html"
+  },
+  {
+    "revision": "6c04598bb5df29c9e7a9",
+    "url": "/react-yt-api-redux/static/css/main.dcb10761.chunk.css"
+  },
+  {
+    "revision": "21e9857dd1ba0bdc49ba",
+    "url": "/react-yt-api-redux/static/js/2.674f880d.chunk.js"
+  },
+  {
+    "revision": "6c04598bb5df29c9e7a9",
+    "url": "/react-yt-api-redux/static/js/main.4caf9138.chunk.js"
+  },
+  {
+    "revision": "a5c6f5c6516032a6cfa5",
+    "url": "/react-yt-api-redux/static/js/runtime~main.e178410e.js"
+  }
+]);
